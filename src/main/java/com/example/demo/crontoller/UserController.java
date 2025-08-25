9651feea41e9f99772f9d73ad3a0663b6698c557
ALUNO <aluno@NOBRELABS.corp>
2025-08-25 16:42:58 -0300

    aplicação base simples

---
+++ src/main/java/com/example/demo/crontoller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.demo.crontoller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/list")
+    public List<User> list(){
+        return userService.list();
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody User user){
+        userService.UserUpdate(user);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@PathVariable long id){
+        userService.UserDelete(id);
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,68 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "User_tbl")
+public class User {
+
+    @Id
+    @Column(name = "Id")
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    //constructor
+    public User(long id, String nome, String email, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+    }
+
+    //getters
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+
+    //setters
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+}
